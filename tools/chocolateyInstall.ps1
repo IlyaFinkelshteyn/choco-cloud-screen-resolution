ea17b034c29b1eea73f18a0e67304ab79b8f1ebe
Dennis Hoer <dennis.hoer@pearson.com>
2018-02-01 11:24:34 -0700

    Output Get-ScheduledTask

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -37,7 +37,9 @@ $ErrorActionPreference = "Stop";
 $TaskName = "CreateRdpHome"
 $Action = New-ScheduledTaskAction -Execute "cmdkey.exe" -Argument "/add:localhost /user:$($pp["username"]) /pass:$($pp["password"])"
 $Settings = New-ScheduledTaskSettingsSet -Priority 1 -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-Register-ScheduledTask -TaskName $TaskName -TaskPath "\" -User $pp["rdpUsername"] -Password $pp['rdpPassword'] -Action $Action -Settings $Settings -RunLevel Highest -Force
+#Register-ScheduledTask -TaskName $TaskName -User $pp["rdpUsername"] -Password $pp['rdpPassword'] -Action $Action -Settings $Settings -RunLevel Highest -Force
+Register-ScheduledTask -TaskName $TaskName -User $pp["rdpUsername"] -Password $pp['rdpPassword'] -Action $Action -Settings $Settings -Force
+#Register-ScheduledTask -TaskName $TaskName -User $pp["rdpUsername"] -Action $Action -Settings $Settings -RunLevel Highest -Force
 
 
 
